86a9405ea2cec9043db981f359bbbdd8e46ff546
Hedayat Abedijoo <h.abedijoo@gmail.com>
2022-05-01 16:59:41 +0200

    decks accept internal move

--- ui/src/components/binering/events.ts
+++ ui/src/components/binering/events.ts
@@ -1,4 +1,5 @@
 export class Events {
   static readonly Card_Removed = 'Card_Removed_Raised';
+  static readonly Card_Moved_To_Another_Deck = 'Card_Moved_To_Another_Deck';
   static readonly First_Started_Round = 'FirstPlayerStartedTheRound';
 }
--- ui/src/components/binering/shiraz/deck.ts
+++ ui/src/components/binering/shiraz/deck.ts
@@ -16,26 +16,102 @@ export class Deck extends LitElement {
   deckId: number = 0;
 
   render() {
-    return html` <div class="deck">
-      ${this.cards.map(
-        (card, i, arr) =>
-          html`<card-comp
-            .playerId=${this.playerId}
-            .value=${card}
-            .deckId=${this.deckId}
-            .draggable=${arr.length - 1 === i ? true : false}
-          >
-          </card-comp>`
-      )}
+    return html` <div
+      class="deck"
+      @drop=${this.droped}
+      @dragover=${this.dragovered}
+      @dragenter=${this.dragEntered}
+      @dragend=${this.dragended}
+      @dragleave=${this.dragleaved}
+    >
+      ${this.cards.length > 0
+        ? this.cards.map(
+            (card, i, arr) =>
+              html`<card-comp
+                .playerId=${this.playerId}
+                .value=${card}
+                .deckId=${this.deckId}
+                .draggable=${arr.length - 1 === i ? true : false}
+              >
+              </card-comp>`
+          )
+        : html`<div class="empty">D</div>`}
     </div>`;
+  }
+  dragended() {
+    this.style.opacity = '1';
+  }
+  dragleaved() {
+    this.style.opacity = '1';
+  }
+  dragEntered(e: any) {
+    var _playerId = e.dataTransfer.types.toString().split('||')[1];
+
+    if (this.playerId == _playerId && this.cards.length < 8) {
+      // if the card is from myself and the Deck has enough spsace, let's add it. It is internal play
+      //  e.preventDefault();
+      this.style.opacity = '0.4';
+    }
   }
   connectedCallback() {
     super.connectedCallback();
     document.addEventListener(Events.Card_Removed, e => this.cardRemoved(e));
+    document.addEventListener(Events.Card_Moved_To_Another_Deck, e =>
+      this.cardedMoved(e)
+    );
   }
   disconnectedCallback() {
     super.disconnectedCallback();
     document.removeEventListener(Events.Card_Removed, this.cardRemoved);
+    document.removeEventListener(
+      Events.Card_Moved_To_Another_Deck,
+      this.cardedMoved
+    );
+  }
+  droped(e: any) {
+    var zero = e.dataTransfer.getData('zero||' + this.playerId);
+    var one = e.dataTransfer.getData('one||' + this.playerId);
+
+    var receivedValue = zero !== '' ? false : true;
+
+    this.cards.push(receivedValue);
+    document.dispatchEvent(
+      new CustomEvent(Events.Card_Moved_To_Another_Deck, {
+        detail: zero !== '' ? zero : one,
+      })
+    );
+    this.requestUpdate();
+    this.style.opacity = '1';
+  }
+  dragovered(e: any) {
+    // do I recieved this drag
+    debugger;
+
+    var _playerId = e.dataTransfer.types.toString().split('||')[1];
+
+    if (this.playerId == _playerId && this.cards.length < 8) {
+      // if the card is from myself and the Deck has enough spsace, let's add it. It is internal play
+      e.preventDefault();
+    }
+  }
+  cardedMoved(e: any) {
+    const dataArray = e.detail.split('||');
+    let playerId = dataArray[0];
+    let deckId = dataArray[1];
+    if (
+      playerId == null ||
+      deckId == null ||
+      playerId == undefined ||
+      deckId == undefined
+    )
+      return;
+
+    if (playerId == this.playerId && this.deckId == deckId) {
+      // remove the save cards at the ends of array.
+      var cardVal = this.cards.pop();
+
+      this.requestUpdate();
+    }
   }
   cardRemoved(e: any) {
     const dataArray = e.detail.split('||');
@@ -70,6 +146,24 @@ export class Deck extends LitElement {
           display: flex;
           height: 200px;
           flex-direction: column;
+        }
+
+        div.empty {
+          height: 100px;
+          width: 60px;
+          flex: 100px 1 0;
+          border-radius: 5px;
+
+          background-color: var(--background-color);
+          box-shadow: 3px 3px 3px gray;
+          color: white;
+          font-size: 2em;
+          border: 3px solid;
+          border-color: gray;
+          -webkit-user-select: none; /* Safari */
+          -moz-user-select: none; /* Firefox */
+          -ms-user-select: none; /* IE10+/Edge */
+          user-select: none; /* Standard */
         }
       `,
     ];
