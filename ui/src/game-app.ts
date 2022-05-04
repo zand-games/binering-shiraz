e99165196cad48c92e7e8ae90ef12ec74c468d40
Hedayat Abedijoo <h.abedijoo@gmail.com>
2022-05-04 10:30:37 +0200

    cleaning up

--- ui/src/game-app.ts
+++ ui/src/game-app.ts
@@ -2,9 +2,6 @@ import '@webcomponents/scoped-custom-element-registry';
 
 import { LitElement, css, html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
-import { AppWebsocket, InstalledCell } from '@holochain/client';
-import { ContextProvider } from '@holochain-open-dev/context';
-import '@material/mwc-circular-progress';
 
 import './components/binering/shiraz/create-game';
 import './components/binering/shiraz/game-detail';
@@ -12,45 +9,13 @@ import './components/binering/shiraz/board-component';
 import './components/message-box';
 import { appWebsocketContext, appInfoContext } from './contexts';
 
-@customElement('holochain-app')
-export class HolochainApp extends LitElement {
-  @state() loading = true;
-  @state() entryHash: string | undefined;
-
-  async firstUpdated() {
-    const appWebsocket = await AppWebsocket.connect(
-      `ws://localhost:${process.env.HC_PORT}`
-    );
-
-    new ContextProvider(this, appWebsocketContext, appWebsocket);
-
-    const appInfo = await appWebsocket.appInfo({
-      installed_app_id: 'binering-app',
-    });
-    new ContextProvider(this, appInfoContext, appInfo);
-
-    this.loading = false;
-  }
-
+@customElement('game-app')
+export class GameApp extends LitElement {
   render() {
-    // if (this.loading)
-    //   return html`
-    //     <mwc-circular-progress indeterminate></mwc-circular-progress>
-    //   `;
-
     return html`
       <main>
-        <h1>Binering Games - Shiraz</h1>
-
+        <h1>Binaring Games - Shiraz</h1>
         <board-component></board-component>
-        <msg-component></msg-component>
-        <!-- <create-game
-          @game-created=${(e: CustomEvent) =>
-          (this.entryHash = e.detail.entryHash)}
-        ></create-game>
-        ${this.entryHash
-          ? html` <game-detail .entryHash=${this.entryHash}></game-detail> `
-          : html``} -->
       </main>
     `;
   }
